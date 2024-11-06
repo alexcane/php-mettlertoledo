20c14c46e7895eb5015a1d250e52f9c5c182cb7c
alexcane <12563984+alexcane@users.noreply.github.com>
2024-11-06 22:00:49 +0100

    updated example

--- Exemple.php
+++ Exemple.php
@@ -1,10 +1,20 @@
 <?php
 
-
 try {
-    $balance = new \PhpMettlerToledo\MTSICS('', 0);
-} catch (\PhpMettlerToledo\Exception\ConnectionException $e) {
+    $scale = new \PhpMettlerToledo\MTSICS('SCALE_IP_ADDRESS', 4305);
+    $commandsArray = $scale->readCommandsAvailable();
+    $weight = $scale->readWeightAndStatus();
+    $tareWeight = $scale->readTareWeight();
+    $netWeight = $scale->readNetWeight();
+    $scale->zeroStable();
+    $scale->zeroImmediately();
+    $scale->tareStable();
+    $scale->tareImmediatly();
+    $scale->clearTare();
+    $firmware = $scale->readFirmwareRevision();
+    $serial = $scale->readSerialNumber();
+    $error = $scale->getError();
 
+} catch (\Exception $e) {
+    echo $e->getMessage();
 }
-$balance->isConnected();
-//$balance->readSerialNumber();
